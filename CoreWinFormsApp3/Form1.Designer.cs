76af28141eec76d2893b3d0268ffc79c0c242f80
clasie <claude_siefers@hotmail.com>
2022-10-22 06:58:49 +0200

    second commit

--- CoreWinFormsApp3/Form1.Designer.cs
+++ CoreWinFormsApp3/Form1.Designer.cs
@@ -29,13 +29,25 @@ namespace CoreWinFormsApp3
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_testConnection = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_testConnection
+            // 
+            this.btn_testConnection.Location = new System.Drawing.Point(204, 110);
+            this.btn_testConnection.Name = "btn_testConnection";
+            this.btn_testConnection.Size = new System.Drawing.Size(291, 29);
+            this.btn_testConnection.TabIndex = 0;
+            this.btn_testConnection.Text = "Test connection";
+            this.btn_testConnection.UseVisualStyleBackColor = true;
+            this.btn_testConnection.Click += new System.EventHandler(this.btn_testConnection_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_testConnection);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -44,6 +56,8 @@ namespace CoreWinFormsApp3
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_testConnection;
     }
 }
 
--- CoreWinFormsApp3/Form1.cs
+++ CoreWinFormsApp3/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,28 @@ namespace CoreWinFormsApp3
             if (!loginUI.IsLogged) {
                 Application.Exit();
             }
+        }
+
+        private void btn_testConnection_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = null;
+            try
+            {
+                string connectionString = "Server=PC1224;DataBase=Drill2;Trusted_Connection=True";
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                MessageBox.Show("Ok connection");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally {
+                if (conn != null) {
+                    conn.Close();
+                }
+            }
+
         }
     }
 }
